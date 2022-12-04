b8e30929b0b23eadb9e24ac5a3f5f7f143f5f4ad
AdelOtmani <Adelatmanikv@gmail.com>
2022-12-04 22:23:49 +0100

    task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 """
-Import modules requests for api data
+Write a Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
 """
 import requests
 from sys import argv
 
 
-def api():
-    """
-    Write a Python script that, using this REST API, for a given employee ID,
-    returns information about his/her TODO list progress.
-    """
+if __name__ == "__main__":
+
     url = requests.get("http://jsonplaceholder.typicode.com/users")
-    for users in url.json():
-        if users.get("id") == int(argv[1]):
-            EMPLOYEE_NAME = (users.get("name"))
-            break
+    TODO = requests.get("http://jsonplaceholder.typicode.com/todos")
+    Data = url.json()
+    Data2 = TODO.json()
     TOTAL_NUM_OF_TASKS = 0
     NUMBER_OF_DONE_TASKS = 0
     TASK_TITLE = []
-    TODO = requests.get("http://jsonplaceholder.typicode.com/todos")
-    for TODOS in TODO.json():
+    for users in Data:
+        if users.get("id") == int(argv[1]):
+            EMPLOYEE_NAME = (users.get("name"))
+  
+    for TODOS in Data2:
         if TODOS.get("userId") == int(argv[1]):
             TOTAL_NUM_OF_TASKS += 1
             if TODO.get("completed") is True:
@@ -29,8 +29,7 @@ def api():
     print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
                                                           NUMBER_OF_DONE_TASKS,
                                                           TOTAL_NUM_OF_TASKS))
-    for TASK in TASK_TITLE:
+    for i in TASK_TITLE:
         print("\t {}".format(TASK))
 
-if __name__ == "__main__":
-    api()
+
