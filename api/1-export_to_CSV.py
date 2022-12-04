efbee4c2ab2d9ba74e0113edf810c2bdfab88fbe
AdelOtmani <Adelatmanikv@gmail.com>
2022-12-04 22:49:16 +0100

    Task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -18,13 +18,13 @@ if __name__ == "__main__":
     TAB = []
     for users in Data:
         if users.get("id") == int(argv[1]):
-            EMPLOYEE_NAME = (users.get("name"))
+            EMPLOYEE_NAME = (users["name"])
     with open(argv[1] + ".csv", "w", encoding="utf-8", newline="") as f:
         W = csv.writer(f, quoting=csv.QUOTE_ALL)
         for TODOS in Data2:
-            if TODOS.get("userId") == int(argv[1]):
-                TAB.append(TODOS.get("userID"))
+            if TODOS["userId"] == int(argv[1]):
+                TAB.append(TODOS["userID"])
                 TAB.append(EMPLOYEE_NAME)
-                TAB.append(TODOS.get("completed"))
-                TAB.append(TODOS.get("title"))
+                TAB.append(TODOS["completed"])
+                TAB.append(TODOS["title"])
                 W.writerow(TAB)
