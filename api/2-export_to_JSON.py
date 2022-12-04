02fccffed2c60367a94e3847691ec7eb1dc6f708
AdelOtmani <Adelatmanikv@gmail.com>
2022-12-04 23:41:40 +0100

    Task 2 identation right

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -17,20 +17,20 @@ if __name__ == "__main__":
 
     for users in Data:
         if users.get("id") == int(argv[1]):
-            EMPLOYEE_NAME = (users["username"])
+            EMPLOYEE_uNAME = (users["username"])
             ID = (users["id"])
 
-        for TODOS in Data2:
-            TAB = {}
+    for TODOS in Data2:
+        TAB = {}
             if TODOS["userId"] == int(argv[1]):
-                TAB["username"] = EMPLOYEE_NAME
+                TAB["username"] = EMPLOYEE_uNAME
                 TAB["task"] = TODOS["title"]
                 TAB["completed"] = TODOS["completed"]
                 tab.append(TAB)
 
-        dict_f = {}
-        dict_f[ID] = tab
-        json_f = json.dumps(dict_f)
+    dict_f = {}
+    dict_f[ID] = tab
+    json_f = json.dumps(dict_f)
 
     with open(argv[1] + ".json", "w", encoding="utf-8") as f:
         f.write(json_f)
