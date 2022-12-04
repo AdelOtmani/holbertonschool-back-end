de85b8991bfb27aea3f3a1f3a769d556fea833b6
AdelOtmani <Adelatmanikv@gmail.com>
2022-12-04 23:26:22 +0100

    Task 2

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Using what you did in the task #0,
+extend your Python script to export data in the CSV format
+"""
+import csv
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = requests.get("http://jsonplaceholder.typicode.com/users")
+    TODO = requests.get("http://jsonplaceholder.typicode.com/todos")
+    Data = url.json()
+    Data2 = TODO.json()
+
+    for users in Data:
+        if users.get("id") == int(argv[1]):
+            EMPLOYEE_NAME = (users["username"])
+            ID = (users["id"])
+
+        for TODOS in Data2:
+            tab=[]
+            TAB = {}
+            if TODOS["userId"] == int(argv[1]):
+                TAB["username"] = EMPLOYEE_NAME
+                TAB["task"] = TODOS["title"]
+                TAB["completed"] = TODOS["completed"]
+                tab.append(TAB)
+        
+        dict_f = {}
+        dict_f[ID] = tab
+        json_f = json.dumb(dict_f)
+
+    with open(argv[1] + ".csv", "w", encoding="utf-8", newline="") as f:
+        f.write(json_f)
