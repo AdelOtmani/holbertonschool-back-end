0094b9ee8b09b0ce4e3dd0d620a3d0e09771e8db
AdelOtmani <Adelatmanikv@gmail.com>
2022-12-04 22:51:33 +0100

    Task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -15,16 +15,17 @@ if __name__ == "__main__":
     TODO = requests.get("http://jsonplaceholder.typicode.com/todos")
     Data = url.json()
     Data2 = TODO.json()
-    TAB = []
     for users in Data:
         if users.get("id") == int(argv[1]):
             EMPLOYEE_NAME = (users["name"])
     with open(argv[1] + ".csv", "w", encoding="utf-8", newline="") as f:
         W = csv.writer(f, quoting=csv.QUOTE_ALL)
         for TODOS in Data2:
+            TAB = []
             if TODOS["userId"] == int(argv[1]):
                 TAB.append(TODOS["userID"])
                 TAB.append(EMPLOYEE_NAME)
                 TAB.append(TODOS["completed"])
                 TAB.append(TODOS["title"])
                 W.writerow(TAB)
+    f.close()
